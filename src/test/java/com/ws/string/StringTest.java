2e31d9b008c465e0147f5b0fbadf37d187c03cb1
yunhua <shun.wang3552@hipac.cn>
2019-08-23 18:21:10 +0800

    string

--- src/test/java/com/ws/string/StringTest.java
+++ src/test/java/com/ws/string/StringTest.java
@@ -1,6 +1,7 @@
 package com.ws.string;
 
 import junit.framework.TestCase;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 /**
@@ -15,6 +16,14 @@ public class StringTest extends TestCase {
         String word = "贝因美纸尿裤";
         String newWord = word.replaceAll("贝因美", "");
         System.out.printf(newWord);
+    }
+
+    @Test
+    public void testString() {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(StringUtils.isBlank(sb.toString()));
+        sb.append("te");
+        System.out.println(StringUtils.isBlank(sb.toString()));
     }
 
 }
