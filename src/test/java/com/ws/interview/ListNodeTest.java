ddac18ddeadbcc638a7d85ae7412071db36e68ab
yunhua <shun.wang3552@hipac.cn>
2020-11-25 11:38:53 +0800

    回文

---
+++ src/test/java/com/ws/interview/ListNodeTest.java
@@ -0,0 +1,79 @@
+package com.ws.interview;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author yunhua
+ * @date 2020-11-25
+ * @see
+ * @since 1.0.0
+ */
+public class ListNodeTest {
+
+    class ListNode {
+        private ListNode next;
+        private int val;
+
+        public ListNode getNext() {
+            return next;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean isPalindrome(ListNode root) {
+        if (Objects.isNull(root) || Objects.isNull(root.next)) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = root;
+        while (temp != null) {
+            list.add(temp.getVal());
+            temp = temp.getNext();
+        }
+        int start = 0;
+        int end = list.size() - 1;
+        while (start <= end) {
+            if (!list.get(start).equals(list.get(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        ListNode root = new ListNode();
+        root.setVal(1);
+        ListNode node2 = new ListNode();
+        node2.setVal(3);
+        root.setNext(node2);
+        ListNode node3 = new ListNode();
+        node3.setVal(3);
+        node2.setNext(node3);
+        ListNode node4 = new ListNode();
+        node4.setVal(1);
+        node3.setNext(node4);
+        /**
+         * 1 3 3 1
+         */
+        System.out.println("是否回文:" + isPalindrome(root));
+    }
+
+}
