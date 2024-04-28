6f9af177bf1b2a7111f39e52e2a07191b232d846
wangshun <shun.wang3552@hipac.cn>
2024-04-28 11:38:42 +0800

    pipeline

--- src/main/java/com/ws/netty/TestPipeline.java
+++ src/main/java/com/ws/netty/TestPipeline.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
  * @since 1.0.0
  */
 @Slf4j
-public class PipelineTest {
+public class TestPipeline {
     public static void main(String[] args) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workGroup = new NioEventLoopGroup(2);
