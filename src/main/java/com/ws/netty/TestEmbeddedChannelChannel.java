18d2b29311dc42d5fa4ac5c6434a69e3481a0b60
wangshun <shun.wang3552@hipac.cn>
2024-04-28 12:31:09 +0800

    EmbeddedChannel

---
+++ src/main/java/com/ws/netty/TestEmbeddedChannelChannel.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018-2022 Hangzhou JuLingShou Intelligent Technology Co., Ltd. All rights reserved
+ */
+package com.ws.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.embedded.EmbeddedChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wangshun
+ * @date 2024-04-28
+ * @see
+ * @since 1.0.0
+ */
+@Slf4j
+public class TestEmbeddedChannelChannel {
+
+    public static void main(String[] args) {
+        ChannelInboundHandlerAdapter h1 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("1:");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelInboundHandlerAdapter h2 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("2:");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelOutboundHandlerAdapter h3 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("3:");
+                super.write(ctx, msg, promise);
+            }
+        };
+        ChannelOutboundHandlerAdapter h4 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("4:");
+                super.write(ctx, msg, promise);
+            }
+        };
+
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel(h1, h2, h3, h4);
+        //embeddedChannel.writeInbound("123");
+        embeddedChannel.writeOutbound("123");
+    }
+}
